f83e8b907453bce494f7099debdd38b4bb048119
Ikeuchi Ryuto <ryu.i7133@gmail.com>
2021-03-26 22:54:30 +0900

    検索フォームをコンポーネント化

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './App.css';
+import { Search } from './components/Search';
+
+export const App=()=> {
+  const [images, setImages] = useState([]);
+  
+
+  return (
+    <div className="App">
+      <Search setImages={ setImages}/>
+
+      <div className="container">
+        {
+          images.map(image => (
+              <div key={image.id} className="card">
+                <img src={image.urls.regular} className="card-img" alt="" />
+                <div className="card-content">
+                  <div className="card-title">
+                    {image.alt_description}
+                  </div>
+                </div>
+                <div className="card-name">
+                  <img src={image.user.profile_image.small} className="card-thumb" alt="" />
+                  <p className="card-author">{image.user.first_name}</p>
+                </div>
+              </div>
+            ))
+        }
+      </div>
+    </div>
+  );
+}
