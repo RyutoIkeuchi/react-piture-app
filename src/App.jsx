a183098278a4e093128be344026561ec945ff5c9
Ikeuchi Ryuto <ryu.i7133@gmail.com>
2021-03-27 15:48:59 +0900

    pretteirでフォーマット化しました

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,31 @@ import React, { useState } from 'react';
 import './App.css';
 import { Search } from './components/Search';
 
-export const App=()=> {
-  const [images, setImages] = useState([]);
-  
+export const App = () => {
+	const [images, setImages] = useState([]);
 
-  return (
-    <div className="App">
-      <Search setImages={ setImages}/>
+	return (
+		<div className="App">
+			<Search setImages={setImages} />
 
-      <div className="container">
-        {
-          images.map(image => (
-              <div key={image.id} className="card">
-                <img src={image.urls.regular} className="card-img" alt="" />
-                <div className="card-content">
-                  <div className="card-title">
-                    {image.alt_description}
-                  </div>
-                </div>
-                <div className="card-name">
-                  <img src={image.user.profile_image.small} className="card-thumb" alt="" />
-                  <p className="card-author">{image.user.first_name}</p>
-                </div>
-              </div>
-            ))
-        }
-      </div>
-    </div>
-  );
-}
+			<div className="container">
+				{images.map((image) => (
+					<div key={image.id} className="card">
+						<img src={image.urls.regular} className="card-img" alt="" />
+						<div className="card-content">
+							<div className="card-title">{image.alt_description}</div>
+						</div>
+						<div className="card-name">
+							<img
+								src={image.user.profile_image.small}
+								className="card-thumb"
+								alt=""
+							/>
+							<p className="card-author">{image.user.first_name}</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,45 +1,45 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-export const Search = ({setImages}) => {
-  const [text, setText] = useState('');
-  const [query, setQuery] = useState('soccer');
+export const Search = ({ setImages }) => {
+	const [text, setText] = useState('');
+	const [query, setQuery] = useState('soccer');
 
-  useEffect(() => {
-    console.log('useEffect')
-    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
-      .then(Response => Response.json())
-      .then(data => {
-        console.log(data)
-        setImages(data.results)
-      });
-  }, [query]);
+	useEffect(() => {
+		console.log('useEffect');
+		fetch(
+			`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`
+		)
+			.then((Response) => Response.json())
+			.then((data) => {
+				console.log(data);
+				setImages(data.results);
+			});
+	}, [query]);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    setQuery(text);
-    setText('');
-    console.log("onSubmit")
-  }
+	const onSubmit = (e) => {
+		e.preventDefault();
+		setQuery(text);
+		setText('');
+		console.log('onSubmit');
+	};
 
-  return (
-    <React.Fragment>
-      <div className="main">
-        <div className="header">
-          <p>search photo app</p>
-        </div>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            onChange={e => setText(e.target.value)}
-            value={text}
-            placeholder="search photos name"
-          />
-          <button type="submit">
-            Search
-          </button>
-        </form>
-        <div className="example">for example:apple,baseball</div>
-      </div>
-    </React.Fragment>
-  );
-}
+	return (
+		<React.Fragment>
+			<div className="main">
+				<div className="header">
+					<p>search photo app</p>
+				</div>
+				<form onSubmit={onSubmit}>
+					<input
+						type="text"
+						onChange={(e) => setText(e.target.value)}
+						value={text}
+						placeholder="search photos name"
+					/>
+					<button type="submit">Search</button>
+				</form>
+				<div className="example">for example:apple,baseball</div>
+			</div>
+		</React.Fragment>
+	);
+};
